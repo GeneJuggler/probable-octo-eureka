94ddbd11e51984892b286f728256385618c4864f
Robert Pollok <rhpollok@gmail.com>
2017-03-19 21:39:58 -0400

    Fixed reversi and added reversegam.py

---
+++ inventwithpython/reversegam.py
@@ -0,0 +1,45 @@
+# Reversegam: a clone of Othello/Reversi
+import random
+import sys
+WIDTH = 8 # Board is 8 spaces wide.
+HEIGHT = 8 # Board is 8 spaces tall.
+def drawBoard(board):
+    # Print the board passed to this function. Return None.
+    print('  12345678')
+    print(' +-------+')
+    for y in range(HEIGHT):
+        print('%s|' % (y+1), end='')
+        for x in range(WIDTH):
+            print(board[x][y], end='')
+        print('|%s' % (y+1))
+    print(' +--------+')
+    print('  12345678')
+
+def getNewBoard():
+    # Create a brand-new, blank board data structure.
+    board = []
+    for i in range(WIDTH):
+        board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+    return board
+
+def isValidMove(board, tile, xstart, ystart):
+    # Return False if the player's move on space xstart, ystart is invalid.
+    # If it is a valid move, return a list of spaces that would become the player's if they made a move here.
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        x += xdirection # First step in the x direction
+        y += ydirection # First step in the y direction
+        while isOnBoard(x, y) and board[x][y] == otherTile:
+            # Keep moving in this x & y direction.
+            x += xdirection
+            y += ydirection
+            if isOnBoard(x, y) and board[x][y] == tile:
