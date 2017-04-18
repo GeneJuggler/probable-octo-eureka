171aa620f269696777412daed0bb47eec642eadb
Robert Pollok <rhpollok@gmail.com>
2017-04-17 21:35:52 -0400

    Trying to learn tkinter

---
+++ tkinter/conversion.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
