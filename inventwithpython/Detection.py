1221b429fffc155d6c29799fdcce592b53272ba6
Robert Pollok <genejuggler@users.noreply.github.com>
2017-05-30 21:11:30 -0400

    Adding Detection.py

---
+++ inventwithpython/Detection.py
@@ -0,0 +1,27 @@
+import pygame, sys, random
+from pygame.locals import *
+
+# Set up pygame.
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# Set up the window.
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Collision Detection')
+
+# Set up the colors.
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+# Set up the player and food data structures.
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+player = pygame.Rect(300, 100, 50, 50)
+foods = []
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+    
