280f704c6d4b16deb61e12147559c194a7f2f277
Robert Pollok <genejuggler@users.noreply.github.com>
2017-06-20 07:02:48 -0400

    Fixing dna.py

--- rosalind/dna/test.py
+++ rosalind/dna/test.py
@@ -1,16 +1,17 @@
 from os import system
 from platform import system as platform
+
 from tkinter import Tk
 from tkinter import filedialog
+
 root = Tk()
 root.withdraw()
+
 if platform() == 'Darwin':  # How Mac OS X is identified by Python
     system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
 else:
     root.lift()
+
 filename = filedialog.askopenfilename()
 
-text = open(filename, 'r')
-text = text.read()
-
-print(text)
+print(filename)
