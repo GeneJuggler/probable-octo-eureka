32952a8a34fd1c7fe7898a348a70d8ff70f8e069
Robert Pollok <rhpollok@Roberts-MacBook-Air.local>
2017-12-03 20:52:45 -0500

    Setting up git on new install

--- rosalind/temp.py
+++ rosalind/temp.py
@@ -1,16 +1,5 @@
 from textImport import getTextFile
 
-n = 5
-k = 3
+dnaseq = getTextFile()
 
-baby_pairs = 1
-adult_pairs = 0
-final_months = 5
-rabbits_litters = 3
-
-month = 0
-
-while month <= final_months:
-    adult_pairs += baby_pairs
-    baby_pairs += (adult_pairs - baby_pairs) * rabbits_litters
-
+print(translate())
