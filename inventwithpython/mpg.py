1e29a5f9bd464fb526bf49758447bfb0feaad032
Robert Pollok <rhpollok@Roberts-MacBook-Air.local>
2018-06-20 19:49:31 -0400

    all of the deleted files registered

--- inventwithpython/mpg.py
+++
@@ -1,4 +0,0 @@
-miles_driven = input("Enter miles driven:")
-gallons_used = input("Enter gallons used:")
-mpg = miles_driven / gallons_used
-print("Miles per gallon:", mpg)
--- rosalind/temp.py
+++ rosalind/temp.py
@@ -2,4 +2,4 @@ from textImport import getTextFile
 
 dnaseq = getTextFile()
 
-print(translate())
+print(dnaseq)
